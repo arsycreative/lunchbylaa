a8f83333b86584fc0c8b8ca04ec9feb28b44b788
Asep Angga Ihza S <angga.asep98@gmail.com>
2025-06-10 13:23:20 +0700

    fix: ensure base URL has trailing slash

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/lunchbylaa/",
+export default defineConfig(({ command }) => ({
+  // serve locally at “/”
+  base: command === "serve" ? "/" : "/lunchbylaa/",
   plugins: [react()],
-});
+}));
