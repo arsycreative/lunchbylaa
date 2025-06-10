1bef1687c2907c653e5e3bb3724678da40e853b3
Asep Angga Ihza S <angga.asep98@gmail.com>
2025-06-10 13:39:11 +0700

    default setting

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  // serve locally at “/”
-  base: command === "serve" ? "/" : "/lunchbylaa/",
+export default defineConfig({
   plugins: [react()],
-}));
+});
